7d502d7a9839556a2f1ca5d2c938e55b1d10a5ea
Mahesh <gattimmahesh@gmail.com>
2023-03-29 10:42:10 +0530

    Initial Commit

---
+++ src/main/java/com/spring/springmvc/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.spring.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public ModelAndView HomePageMethod() {
+		ModelAndView model = new ModelAndView("home");
+		return model;
+	}
+
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home Page</title>
+</head>
+<body>
+   <h1>This is my first Spring mvc project</h1>
+   <h1>I am learning Spring mvc from telugu web guru channel</h1>
+</body>
+</html>
